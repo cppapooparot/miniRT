08ade53634a06c2c97a6e54142bc7404fe3e27d7
Narek <nsahakyan42@gmail.com>
2026-02-01 00:29:22 +0400

    add intersect cylinder's disk function

--- src/intersections/ray_cylinder.c
+++ src/intersections/ray_cylinder.c
@@ -6,7 +6,32 @@ static t_vec3   project_on_axis(t_vec3 v, t_vec3 axis)
     return (vec3_scale(axis, vec3_dot(v, axis)));
 }
 
-static bool intersect_disk(t_ray ray, ...);
+static bool intersect_disk(t_ray ray, t_vec3 disk_center, t_vec3 disk_normal, double radius, double *t_hit)
+{
+    double denom;
+    double t;
+    t_vec3 p;
+    t_vec3 v;
+    t_vec3 in_plane;
+    double dist_square;
+
+    if (!t_hit)
+        return (false);
+    denom = vec3_dot(ray.direction, disk_normal);
+    if (fabs(denom) < EPSILON)
+        return (false);
+    t = vec3_dot(vec3_subtract(disk_center, ray.origin), disk_normal) / denom;
+    if (t < EPSILON)
+        return (false);
+    p = vec3_add(ray.origin, vec3_scale(ray.direction, t));
+    v = vec3_subtract(p, disk_center);
+    in_plane = vec3_subtract(v, project_on_axis(v, disk_normal));
+    dist_square = vec3_length_squared(in_plane);
+    if (dist_square > radius * radius)
+        return (false);
+    *t_hit = t;
+    return (true);
+}
 
 bool intersect_cylinder(t_ray ray, t_cylinder *cylinder, double *t)
 {
@@ -14,4 +39,6 @@ bool intersect_cylinder(t_ray ray, t_cylinder *cylinder, double *t)
     double  half_height;
     t_vec3  axis;
     t_vec3  oc;
+    
+    return (true);
 }
