ad52a9dc5b8b43dbfe89af1fa457e644b1182266
Mikayel Aghumyan <mikaelaghumyan@outlook.com>
2026-02-02 01:05:02 +0400

    feat: update intersection structures and add light calculation functions

--- inc/intersections.h
+++ inc/intersections.h
@@ -1,46 +1,50 @@
 #ifndef INTERSECTIONS_H
-#define INTERSECTIONS_H
+# define INTERSECTIONS_H
 
+# include "../libft/libft.h"
 # include "scene.h"
 # include "vec3d.h"
 # include <stdbool.h>
 
 typedef enum e_hit_type
 {
-    HIT_NONE = 0,
-    HIT_SPHERE,
-    HIT_PLANE,
-    HIT_CYLINDER
-}   t_hit_type;
+	HIT_NONE = 0,
+	HIT_SPHERE,
+	HIT_PLANE,
+	HIT_CYLINDER
+}				t_hit_type;
 
 typedef struct s_hit_record
 {
-    bool        hit;
-    double      t; // ray parameter
-    t_vec3      point;
-    t_vec3      normal;
-    t_rgb       color;
-    t_hit_type  type;
-}   t_hit_record;
+	bool		hit;
+	double t; // ray parameter
+	t_vec3		point;
+	t_vec3		normal;
+	t_rgb		color;
+	t_hit_type	type;
+	t_cylinder	*cylinder;
+	t_sphere	*sphere;
+	t_plane		*plane;
+}				t_hit_record;
 
 typedef struct s_sphere_intersect
 {
-    double              epsilon;
-    t_vec3			    oc;  // vector from centre of the sphere to start of the ray
-    double			    a;
-    double			    b;
-    double			    c;
-    double			    discriminant;
-    double			    sqrt_d;
-    double			    t0;
-    double			    t1;
-}   t_sphere_intersect;
+	double		epsilon;
+	t_vec3 oc; // vector from centre of the sphere to start of the ray
+	double		a;
+	double		b;
+	double		c;
+	double		discriminant;
+	double		sqrt_d;
+	double		t0;
+	double		t1;
+}				t_sphere_intersect;
 
-bool    intersect_sphere(t_ray ray, t_sphere *sphere, double *t);
-bool    intersect_plane(t_ray ray, t_plane *plane, double *t);
-bool    intersect_cylinder(t_ray ray, t_cylinder *cylinder, double *t);
+bool			intersect_sphere(t_ray ray, t_sphere *sphere, double *t);
+bool			intersect_plane(t_ray ray, t_plane *plane, double *t);
+bool			intersect_cylinder(t_ray ray, t_cylinder *cylinder, double *t);
 
-bool    find_closest_intersection(t_ray ray, t_scene *scene, t_hit_record *rec);
+bool			find_closest_intersection(t_ray ray, t_scene *scene,
+					t_hit_record *rec);
 
 #endif
-
--- src/lightning/phong.c
+++ src/lightning/phong.c
@@ -6,18 +6,46 @@
 /*   By: maghumya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 18:38:34 by maghumya          #+#    #+#             */
-/*   Updated: 2026/01/30 01:15:13 by maghumya         ###   ########.fr       */
+/*   Updated: 2026/01/31 19:46:25 by maghumya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../inc/scene.h"
-#include "../../inc/vec3d.h"
+#include "../../inc/intersections.h"
 
-t_rgb	calculate_ambient(t_scene *scene, t_rgb object_color)
+t_rgb	calculate_ambient(t_scene *scene, t_hit_record *record)
 {
 	t_rgb	ambient_color;
 
-	ambient_color = rgb_multiply(scene->ambient_light, object_color);
+	ambient_color = rgb_multiply(scene->ambient_light, record->color);
 	ambient_color = rgb_scale(ambient_color, scene->ambient_intensity);
 	return (ambient_color);
+}
+
+t_rgb	calculate_diffuse(t_light *light, t_hit_record *record)
+{
+	t_vec3	light_dir;
+	double	diffuse_intensity;
+	t_rgb	diffuse_color;
+
+	light_dir = vec3_normalize(vec3_subtract(light->position, record->point));
+	diffuse_intensity = fmax(0.0, vec3_dot(record->normal, light_dir));
+	diffuse_color = rgb_multiply(light->color, record->color);
+	diffuse_color = rgb_scale(diffuse_color, diffuse_intensity
+			* light->brightness);
+	return (diffuse_color);
+}
+
+t_rgb	calculate_light(t_scene *scene, t_hit_record *record)
+{
+	t_rgb final_color;
+	t_list *lights = scene->lights;
+
+	final_color = calculate_ambient(scene, record);
+	while (lights)
+	{
+		t_light *light = (t_light *)lights->content;
+		final_color = rgb_add(final_color, calculate_diffuse(light, record));
+		lights = lights->next;
+	}
+	return (rgb_clamp(final_color));
 }
